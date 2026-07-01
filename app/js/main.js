da08620f5372ea2d38ad80d472110aa5ec3f2542
Jhauyon <jjauyon.avanzadi@outlook.com>
2026-07-01 21:49:25 +0200

    Widget flotante de soporte con envio por correo

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 import { closeSidebarBtn, openSidebarBtn } from "./modules/selectores.js";
 import { closeSidebar, openSidebar, toggleTabIndex } from "./modules/components/Sidebar.js";
+import "./modules/components/Soporte.js"; // widget flotante de soporte (se autoinicializa)
 
 document.addEventListener("DOMContentLoaded", () => {
     toggleTabIndex();
